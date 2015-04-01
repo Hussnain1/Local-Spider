6246a2e96a893e762e8d5101eb40e8bca3606c85
Hussnain Cheema <hussnain_93@hotmail.com>
2015-04-02 00:20:07 +0500

    Create UnitTest.java

---
+++ UnitTest.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UnitTest {
+
+	public static void main(String Args[]) {
+
+		webCrawler myObj = new webCrawler();
+		myObj.myfunc();
+	}
+}
